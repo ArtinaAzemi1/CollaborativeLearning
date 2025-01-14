5f84c7cc52e5f0da99744ed5fb10d48cd4da1a91
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-14 12:40:34 +0100

    Ndryshime tek student dhe group

--- src/data/Group.js
+++ src/data/Group.js
@@ -1,5 +1,10 @@
+import React, {useState, useEffect} from 'react';
+
 const Group =() => {
-    
+    return (
+        <>
+        </>
+    )
 }
 
 export default Group;
--- src/data/Student.js
+++ src/data/Student.js
@@ -1,5 +1,10 @@
+import React, {useState, useEffect} from 'react';
+
 const Student =() => {
-    
+    return (
+        <>
+        </>
+    )
 }
 
 export default Student;
